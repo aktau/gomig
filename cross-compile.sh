c2e8313c9babeddfa18dfb29f9b35582f591e0c1
Nicolas Hillegeer <nicolas@hillegeer.com>
2013-12-20 22:04:38 +0100

    make cross-compiling more useful
    it now generates binaries!

--- cross-compile.sh
+++ cross-compile.sh
@@ -1,17 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-echo '# linux arm7'
-GOARM=7 GOARCH=arm GOOS=linux go build
-echo '# linux arm5'
-GOARM=5 GOARCH=arm GOOS=linux go build
-echo '# windows 386'
-GOARCH=386 GOOS=windows go build
-echo '# windows amd64'
-GOARCH=amd64 GOOS=windows go build
-echo '# darwin'
-GOARCH=amd64 GOOS=darwin go build
-echo '# freebsd'
-GOARCH=amd64 GOOS=freebsd go build
+BIN="./bin"
+APP="gomig"
+APP_PATH="$BIN/$APP"
 
+[ -d "$BIN" ] || mkdir "$BIN"
+
+echo '# linux arm7'
+GOARM=7 GOARCH=arm GOOS=linux go build -o "$APP_PATH"-arm5
+echo '# linux arm5'
+GOARM=5 GOARCH=arm GOOS=linux go build -o "$APP_PATH"-arm7
+echo '# windows 386'
+GOARCH=386 GOOS=windows go build -o "$APP_PATH"-x86.exe
+echo '# windows amd64'
+GOARCH=amd64 GOOS=windows go build -o "$APP_PATH"-x64.exe
+echo '# darwin'
+GOARCH=amd64 GOOS=darwin go build -o "$APP_PATH"-osx
+echo '# freebsd'
+GOARCH=amd64 GOOS=freebsd go build -o "$APP_PATH"-freebsd
